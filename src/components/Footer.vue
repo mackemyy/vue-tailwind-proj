80184d5e9c3e270bd0b4d5a8c22164fb014cab28
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-01-20 19:01:25 +0800

    Create Footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Footer1 from './Footer1.vue';
+
+
+    export default {
+        name:'FootersVue',
+        components: {
+            Footer1,
+        }
+    }
+</script>
+
+<template>
+    <Footer1/>
+</template>
