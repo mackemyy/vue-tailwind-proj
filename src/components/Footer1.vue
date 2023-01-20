ea006a056bd7b55930f8e99c81375343e8607877
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-01-20 20:33:40 +0800

    Adjust padding in Footer1

--- src/components/Footer1.vue
+++ src/components/Footer1.vue
@@ -6,7 +6,7 @@
 
 <template>
     <div class="bg-gradient-to-r from-blue-600 to-cyan-300">
-        <div class="flex items-center justify-center gap-[20rem] py-11">
+        <div class="flex items-center justify-center gap-[20rem] py-12">
             <p class="font-inter text-white font-semibold text-[32px]">Ready to get started?<br/>Get in touch and Join Us!</p>
             <div>
                 <button class="bg-dirtyWhite text-darkBlue rounded-[16px] font-bold font-inter text-[24px] py-4 px-14 inline-flex items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...">
