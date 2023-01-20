6cbfc1e02e1bce2442fbb3f8da729d397d1dea8e
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-01-20 18:34:25 +0800

    Create WhyAxis Component

---
+++ src/components/WhyAxis.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export default {
+        name: 'WhyAxisVue'
+    }
+</script>
+
+<template>
+    <div class="relative flex">
+        <img src="src\assets\bg1.svg" alt="">
+        <div class="absolute pt-11 pl-[22rem] ">
+                <p class="text-white font-inter font-semibold text-[48px] pb-3">Why Axis</p>
+                <p class=" text-white font-inter text-base font-normal tracking-normal">Aside that it’s <b>FREE</b>, we are top notched software making sure accessiblity and<br/> speed is well provided. Additional features like Syllabus and Grading makes your<br/> management easier. More features with <b>easier interface</b>.</p>
+        </div>
+        <div class="absolute pt-[17rem] pl-[22rem] ">
+            <div class="columns-2 gap-[24rem]">
+                <p class="text-lightBlue font-inter font-semibold text-[32px]">249+</p>
+                <p class="text-lightBlue font-inter font-semibold text-[32px]">5 years</p>
+            </div>
+        </div>
+        <div class="absolute pt-[19rem] pl-[22rem] ">
+            <div class="columns-2 pt-4 gap-[12rem]">
+                <p class="text-white font-inter font-normal text-[16px]">We are former Whitebelt. These Schools<br/> trusted us for 5 years.</p>
+                <p class="text-white font-inter font-normal text-[16px]">In Martial Arts Business software<br/> providing high maintenance and security</p>
+            </div>
+        </div>
+        <div class="absolute pt-[27rem] pl-[22rem] ">
+            <div class="columns-2 gap-[22.7rem]">
+                <p class="text-lightBlue font-inter font-semibold text-[32px]">98%</p>
+                <p class="text-lightBlue font-inter font-semibold text-[32px]">50,000+</p>
+            </div>
+        </div>
+        <div class="absolute pt-[29rem] pl-[22rem] ">
+            <div class="columns-2 pt-4 gap-[11rem]">
+                <p class="text-white font-inter font-normal text-[16px]">Happy users based on our yearly survey.<br/> We are here to even improve our app more.</p>
+                <p class="text-white font-inter font-normal text-[16px]">Students Logged in over the last 5 years.</p>
+            </div>
+        </div>
+    </div>
+
+</template>
