7669f0ede92ca348f3ae695cb8dbc029133538c3
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-01-20 17:34:11 +0800

    Create FeatureTextContent Component

---
+++ src/components/FeatureTextContent.vue
@@ -0,0 +1,96 @@
+<script lang="ts">
+    export default {
+        name:'FeatureTextContentVue',
+    }
+</script>
+
+<template>
+    <div class="w-auto h-[53rem] bg-lightBlue">
+        <div>
+            <p class="font-semibold font-inter text-5xl tracking-wide text-white flex justify-center pt-[4.5rem]">Our Features</p>
+            <div class="ml-[22em] mr-[14em]">
+                <div class="columns-3 pt-16">
+                    <div>
+                        <img src="src\assets\icon1.svg" alt=""/>
+                        <img src="src\assets\icon2.svg" alt=""/>
+                        <img src="src\assets\icon3.svg" alt=""/>
+                    </div>
+                </div>
+                <div class="columns-3 pt-4">
+                    <div>
+                        <p class="font-inter font-bold text-white text-lg">Dashboard</p>
+                        <p class="font-inter font-bold text-white text-lg">Student & Lead Profile</p>
+                        <p class="font-inter font-bold text-white text-lg">Programs and Ranks</p>
+                    </div>
+                </div>
+                <div class="columns-3 pt-4">
+                    <div>
+                        <ul role="list" class="marker:text-white list-disc pl-5 space-y-1 text-white font-inter font-normal text-base tracking-wide">
+                            <li>Student & Lead Attendance</li>
+                            <li>Membership Status</li>
+                            <li>Program Status</li>
+                            <li>Heat Map</li>
+                            <li>Birthday Tracker</li>
+                        </ul>
+                        <ul role="list" class="marker:text-white list-disc pl-5 space-y-1 text-white font-inter font-normal text-base tracking-wide">
+                            <li >Daily Attendance</li>
+                            <li>Performance Status</li>
+                            <li>Grade Management</li>
+                            <li>Membership Status</li>
+                            <li>Digital Agreements</li>
+                        </ul>
+                        <ul role="list" class="marker:text-white list-disc pl-5 space-y-1 text-white font-inter font-normal text-base tracking-wide">
+                            <li >Custom Programs</li>
+                            <li>Subranks</li>
+                            <li>Grade Management</li>
+                            <li>Membership STatus</li>
+                            <li>Digital Attendance</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="columns-3 pt-16">
+                    <div>
+                        <img src="src\assets\icon1.svg" alt=""/>
+                        <img src="src\assets\icon4.svg" alt=""/>
+                        <img src="src\assets\icon5.svg" alt=""/>
+                    </div>
+                </div>
+                <div class="columns-3 pt-4">
+                    <div>
+                        <p class="font-inter font-bold text-white text-lg">Manage Grading System</p>
+                        <p class="font-inter font-bold text-white text-lg">Syllabus</p>
+                        <p class="font-inter font-bold text-white text-lg">Attendance Tracker</p>
+                    </div>
+                </div>
+                <div class="columns-3 pt-4">
+                    <div>
+                        <ul role="list" class="marker:text-white list-disc pl-5 space-y-1 text-white font-inter font-normal text-base tracking-wide">
+                            <li >Student & Lead Attendance</li>
+                            <li>Membership Status</li>
+                            <li>Program Status</li>
+                            <li>Heat Map</li>
+                            <li>Birthday Tracker</li>
+                        </ul>
+                        <ul role="list" class="marker:text-white list-disc pl-5 space-y-1 text-white font-inter font-normal text-base tracking-wide">
+                            <li >Daily Attendance</li>
+                            <li>Performance Status</li>
+                            <li>Grade Management</li>
+                            <li>Membership Status</li>
+                            <li>Digital Agreements</li>
+                        </ul>
+                        <ul role="list" class="marker:text-white list-disc pl-5 space-y-1 text-white font-inter font-normal text-base tracking-wide">
+                            <li >Custom Programs</li>
+                            <li>Subranks</li>
+                            <li>Grade Management</li>
+                            <li>Membership STatus</li>
+                            <li>Digital Attendance</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
