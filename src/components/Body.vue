8f944e44139964ca35dc1f8a905276c77827bd7a
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-01-20 13:01:35 +0800

    Utilize Button Component on Body

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -18,8 +18,7 @@ import ButtonsVue from './Buttons.vue';
                 <p class="text-white font-inter font-semibold text-5xl leading-6 tracking-wide">The Only <b><i>FREE</i></b><br/>Martial Arts Software<br/>in the World</p>
                 <p class="text-white font-inter font-medium text-base leading-6 mt-3 mb-5">Make management easy and friendly with Axis. Track your<br/> school’s performance, student’s progress and a lot more in<br/> just few clicks <b>without paying per month!</b></p>
                 <div>
-                    <ButtonsVue :buttonText="'Join Now'" :btnColor="'white'" :textColor="'darkerBlue'" :yvalue="3" :xvalue="11"/>
-                    <!-- <button class="bg-white text-blue-900 px-11 py-3 rounded-[0.6rem] font-inter font-bold">Join Now!</button> -->
+                    <ButtonsVue :buttonText="'Join Now'" :btnColor="'white'" :textColor="'darkBlue'" :yvalue="3" :xvalue="11"/>
                 </div>
             </div>
             <img class="pt-[3.2rem] max-w-[35rem] mx-auto" src="src\assets\karategirl.svg" />
